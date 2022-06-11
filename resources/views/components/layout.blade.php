21c0bd548c79171ee8c66f40e32a9563e4072cbd
jpoliveira08 <joaopedro.oliveirajava@gmail.com>
2022-06-11 15:44:16 -0300

    Added the first version of the index page

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="{{ asset('/css/surveys/index.css') }}" rel="stylesheet">
+    <link href="{{ asset('/css/surveys/buttons.css') }}" rel="stylesheet">
+    <link href="{{ asset('/css/surveys/table.css') }}" rel="stylesheet"> 
+    <title>{{ $title }}</title>
+</head>
+<body>
+    {{ $slot }}
+</body>
+</html>
+
+<script src="{{ asset('js/surveys/index.js')}}"></script>
--- resources/views/surveys/index.blade.php
+++ resources/views/surveys/index.blade.php
@@ -1,5 +1,39 @@
-@foreach($surveys as $survey)
+<x-layout title="Enquetes">
 
-    {{$survey->title}}
+    <div class="container center">
+        <h1>Enquetes</h1>
+    </div>
 
-@endforeach
+    <div class="container space-around">
+        <a href="" class="btn btn-primary">Nova enquete</a>
+
+        <a href="" class="btn btn-info">Filtrar</a>
+    </div>
+    
+    <div class="container column">
+        <table>
+            <thead>
+                <tr>
+                    <th>Título</th>
+                    <th>Inicia em</th>
+                    <th>Expira em</th>
+                    <th>Ação</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($surveys as $survey)
+                <tr>
+                    <td class="column-title">{{ $survey->title }}</td>
+                    <td>{{ $survey->begins_in }}</td>
+                    <td>{{ $survey->expires_in }}</td>
+                    <td>
+                        <a class="btn btn-warning" href="">Editar</a>
+                        <a class="btn btn-danger" href="">Excluir</a>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+
+</x-layout>
